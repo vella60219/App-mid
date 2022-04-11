f3f88b7658b57476ab03d1b77260c8c056d3229b
vella60219 <83958906+vella60219@users.noreply.github.com>
2022-04-12 00:46:47 +0800

    HomeScreen ActionButton fix

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,6 +6,7 @@ import { UserHomeProfile } from '../components/User';
 import RecentEatItem from '../components/RecentEatItem';
 import ItemList from '../components/RecentEatList';
 
+import ActionButton from "../components/ActionButton";
 
 import users from "../json/user.json";
 import MyTheme from "../Theme";
@@ -13,26 +14,29 @@ import MyTheme from "../Theme";
 const HomeScreen = ({ navigation }) => {
     let user = users[0];
     return (
-        <ScrollView 
-            theme={MyTheme}
-            _light={{bg:"white"}}
-            _dark={{bg:"black"}}
-        >
-            <UserHomeProfile 
-                user={users[0]}
-            />
-             {/* <RecentEatItem
-                user={users[0]}
-            /> */}
-            {/* <ItemList/> */}
+        <>
+            <ScrollView 
+                theme={MyTheme}
+                _light={{bg:"white"}}
+                _dark={{bg:"black"}}
+            >
+                <UserHomeProfile 
+                    user={users[0]}
+                />
+                {/* <RecentEatItem
+                    user={users[0]}
+                /> */}
+                {/* <ItemList/> */}
 
 
-            <Pressable
-                onPress={() => navigation.navigate('Shop')}
-                shadow= {2}
-                h={100} w={100} bg="primary"
-            ></Pressable>
-        </ScrollView>
+                <Pressable
+                    onPress={() => navigation.navigate('Shop')}
+                    shadow= {2}
+                    h={100} w={100} bg="primary"
+                ></Pressable>
+            </ScrollView>
+            <ActionButton navigation={navigation} />
+        </>
     );
 }
 
