6af0ba670adadc0da1bcc6df4ded162fbbbd4e04
vella60219 <83958906+vella60219@users.noreply.github.com>
2022-04-14 00:03:46 +0800

    bug fux

--- src/screens/ShopDetailScreen.js
+++ src/screens/ShopDetailScreen.js
@@ -19,25 +19,27 @@ const ShopDetailScreen = ({ navigation, route }) => {
     var shop = user.shop_data[key-1];
 
     return (
+        <>
         <Box
             theme={MyTheme}
             _light={{bg: colors.white }}
             _dark={{bg: colors.black}}
+            h="100%"
         >
                 <Center>
                     <Text>{shop.shop_name}</Text>
                     <Text>{shop.introduction}</Text>
-                    <TagList navigation={navigation} sections={shop.tag_data} edit={true} bg={false} />
                 </Center>
+                    <TagList navigation={navigation} sections={shop.tag_data} edit={true} bg={false} />
                     <Box flex={1} height={800}>
                     <TopTab navigation={navigation} shop={shop_id} mode="show" />
                     </Box>
-                    
                 <ActionButton
                     navigation={navigation}
                     mode="edit"
                 />
         </Box>
+        </>
     );
 }
 
